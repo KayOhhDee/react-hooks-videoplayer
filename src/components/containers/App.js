d1c694abbd8da9e9a05809d1474788ecc0a79989
kod3180 <kod3180@gmail.com>
2020-03-15 19:24:17 +0000

    router set up

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Player from './Player';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 const App = () => (
-  <Player />
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Player} />
+      <Route exact path="/:currentVideo" component={Player} />
+    </Switch>
+  </BrowserRouter>
 )
 
 export default App;
